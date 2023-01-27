7faf4f22e7d84eed4f8376f4a294a5409c869def
Grayson <48541040+myaccunt@users.noreply.github.com>
2023-01-27 20:22:15 -0300

    Update recover.py

--- recover.py
+++ recover.py
@@ -1,15 +1,17 @@
 import os
 import shutil
+import tqdm
 
 # Define o caminho do disco a ser varrido
 path = "C:\\"
 
 # Faz a varredura do disco
-for root, dirs, files in os.walk(path):
+for root, dirs, files in tqdm.tqdm(os.walk(path)):
     for file in files:
         # Verifica se o arquivo foi apagado
         if file.endswith("~"):
-            # Recupera o arquivo
-            source = os.path.join(root, file)
-            destination = os.path.join("D:\\recovered_files", file)
-            shutil.move(source, destination)
+            if file.endswith((".jpg", ".jpeg", ".png",".3gp", ".bmp", ".mp4", ".avi", ".mkv", ".wmv", ".flv", ".mov")):
+                # Recupera o arquivo
+                source = os.path.join(root, file)
+                destination = os.path.join("D:\\recovered_files", file)
+                shutil.move(source, destination)
